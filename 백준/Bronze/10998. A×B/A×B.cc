5cb2df5ec409176d3b295d7465e837ab1b92831d
Mini <151009045+BcKmini@users.noreply.github.com>
2024-11-09 16:49:03 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    // 변수 선언
+    int A, B;
+
+    // 두 정수 A와 B를 입력 받기
+    std::cin >> A >> B;
+
+    // A와 B의 곱을 계산
+    int result = A * B;
+
+    // 결과 출력
+    std::cout << result << std::endl;
+
+    return 0;
+}
